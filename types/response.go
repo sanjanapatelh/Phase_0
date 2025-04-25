80910a287c816c704620ee7894f1d1fcee00d76e
yashvarma2001 <yashvarma2001@gmail.com>
2025-04-25 15:51:18 -0400

    Minor changes

--- types/response.go
+++ types/response.go
@@ -11,13 +11,13 @@ type Response struct {
 	Status    Code        `json:"status"`
 	Val       interface{} `json:"val"`
 	Uid       string      `json:"uid"`
-	Writers   []string    `json:"writers,omitempty"`
-	Readers   []string    `json:"readers,omitempty"`
-	Copytos   []string    `json:"copytos,omitempty"`
-	Copyfroms []string    `json:"copyfroms,omitempty"`
-	Indirects []string    `json:"indirects,omitempty"`
-	R_k       []string    `json:"r(k),omitempty"`
-	W_k       []string    `json:"w(k),omitempty"`
-	C_Src_k   []string    `json:"c_src(k),omitempty"`
-	C_Dst_k   []string    `json:"c_dst(k),omitempty"`
+	Writers   []string    `json:"writers"`
+	Readers   []string    `json:"readers"`
+	Copytos   []string    `json:"copytos"`
+	Copyfroms []string    `json:"copyfroms"`
+	Indirects []string    `json:"indirects"`
+	R_k       []string    `json:"r(k)"`
+	W_k       []string    `json:"w(k)"`
+	C_Src_k   []string    `json:"c_src(k)"`
+	C_Dst_k   []string    `json:"c_dst(k)"`
 }
